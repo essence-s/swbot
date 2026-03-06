32aea275a76676cb6d92ed96bac440390549d2f5
styv <styv2301@gmail.com>
2026-03-06 13:46:12 -0500

    chore: aplicar las reglas de formato de Prettier

--- lbSbot/users.ts
+++ lbSbot/users.ts
@@ -1,86 +1,86 @@
 export type User = {
-	flowCurrent: string;
-	currentSection: number;
-	nameSubFlow: string;
-	isProcessing?: boolean;
-	data?: any;
+  flowCurrent: string;
+  currentSection: number;
+  nameSubFlow: string;
+  isProcessing?: boolean;
+  data?: any;
 };
 
 type UserId = string | number;
 
 const users: Record<UserId, User> = {
-	987654321: {
-		flowCurrent: '',
-		currentSection: 0,
-		nameSubFlow: '',
-		isProcessing: false,
-	},
+  987654321: {
+    flowCurrent: '',
+    currentSection: 0,
+    nameSubFlow: '',
+    isProcessing: false,
+  },
 };
 
 function isProcessing(userId: string) {
-	return users[userId]?.isProcessing;
+  return users[userId]?.isProcessing;
 }
 
 const startProcessing = (userId: string) => {
-	users[userId].isProcessing = true;
+  users[userId].isProcessing = true;
 };
 
 const stopProcessing = (userId: string) => {
-	users[userId].isProcessing = false;
+  users[userId].isProcessing = false;
 };
 
 const saveCurretSection = (
-	userId: string,
-	nameSubFlow: string,
-	flowLength: number
+  userId: string,
+  nameSubFlow: string,
+  flowLength: number
 ) => {
-	let user = users[userId];
+  let user = users[userId];
 
-	console.log(flowLength);
-	if (user && user.currentSection !== undefined) {
-		console.log(user.currentSection >= flowLength - 1);
-		if (user.currentSection >= flowLength - 1) {
-			user.currentSection = 0;
-			user.flowCurrent = '';
-			user.nameSubFlow = '';
-			return;
-		}
-		user.currentSection = user.currentSection + 1;
-		user.nameSubFlow = nameSubFlow;
-	} else {
-		users[userId] = { currentSection: 0, flowCurrent: '', nameSubFlow: '' };
-	}
-	// return users
+  console.log(flowLength);
+  if (user && user.currentSection !== undefined) {
+    console.log(user.currentSection >= flowLength - 1);
+    if (user.currentSection >= flowLength - 1) {
+      user.currentSection = 0;
+      user.flowCurrent = '';
+      user.nameSubFlow = '';
+      return;
+    }
+    user.currentSection = user.currentSection + 1;
+    user.nameSubFlow = nameSubFlow;
+  } else {
+    users[userId] = { currentSection: 0, flowCurrent: '', nameSubFlow: '' };
+  }
+  // return users
 };
 
 const setNameSubFlow = (userId: string, nameSubFlow: string) => {
-	let user = users[userId];
-	if (user) {
-		user.nameSubFlow = nameSubFlow;
-	} else {
-		// users[number] = { currentSection: 0, flowCurrent: '', nameSubFlow: nameSubFlow };
-	}
+  let user = users[userId];
+  if (user) {
+    user.nameSubFlow = nameSubFlow;
+  } else {
+    // users[number] = { currentSection: 0, flowCurrent: '', nameSubFlow: nameSubFlow };
+  }
 };
 
 const stableCurrent = (userId: string) => {
-	let user = users[userId];
+  let user = users[userId];
 
-	if (!user) {
-		users[userId] = { currentSection: 0, flowCurrent: '', nameSubFlow: '' };
-	}
+  if (!user) {
+    users[userId] = { currentSection: 0, flowCurrent: '', nameSubFlow: '' };
+  }
 };
 
 const getCurrent = (userId: string) => {
-	let user = users[userId];
-	return user;
+  let user = users[userId];
+  return user;
 };
 
 export {
-	isProcessing,
-	startProcessing,
-	stopProcessing,
-	saveCurretSection,
-	stableCurrent,
-	getCurrent,
-	setNameSubFlow,
+  isProcessing,
+  startProcessing,
+  stopProcessing,
+  saveCurretSection,
+  stableCurrent,
+  getCurrent,
+  setNameSubFlow,
 };
